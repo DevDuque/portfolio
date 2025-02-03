20b05e431e5d2cfaf28d0af491e3c782e68706a3
DevDuque <devgduque@gmail.com>
2025-02-03 14:50:01 -0300

    Add the assets and rename the front-end project

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import './global.css'
+
+function App() {
+
+  return (
+    <>
+      <h2> Duque</h2>
+
+      <Header />
+    </>
+  )
+}
+
+export default App
