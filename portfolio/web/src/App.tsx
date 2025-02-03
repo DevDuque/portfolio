a053ba81ef716e2fcc7b1f04c167366ebe71c733
DevDuque <devgduque@gmail.com>
2025-02-03 13:21:41 -0300

    Add the initial state of the project

---
+++ portfolio/web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-background h-full w-full">
+      
+      <h1 className="text-3xl text-text-color"> DevDuque </h1>
+    </div>
+  )
+}
+
+export default App
