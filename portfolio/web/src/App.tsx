b34de4c9b5af103b2471ce39d7a2cf61d548d834
DevDuque <devgduque@gmail.com>
2025-02-03 14:45:26 -0300

    Add the assets and rename the front-end project

--- portfolio/web/src/App.tsx
+++ portfolio/web/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import './global.css'
 
 function App() {
 
   return (
-    <div className="bg-background h-full w-full">
+    <div className="bg-background h-100% w-full">
       
       <h1 className="text-3xl text-text-color"> DevDuque </h1>
     </div>
